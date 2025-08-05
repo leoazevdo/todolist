dc39ce18bc5b06f9fb0d0eeec9dae81ec984aa96
leoazevdo <discopositivoleo@gmail.com>
2025-08-04 21:50:24 -0300

    Crindo function de buscar tarefas

--- index.js
+++ index.js
@@ -8,3 +8,30 @@ function fecharNovaTarefa() {
  overlay.classList.remove('active');
  novatarefa.classList.remove('active');
 }
+
+function buscarTarefas() {
+  fetch("http://localhost:3000/tarefas")
+    .then(response => response.json())
+    .then(response => {
+      adicionarTarefa(response);
+    })
+} buscarTarefas();
+
+function adicionarTarefa(listaDeTarefas) {
+  if (listaDeTarefas.length > 0) {
+    lista.innerHTML = "";
+    listaDeTarefas.map(tarefa => {
+      lista.innerHTML += `
+      <li>
+        <h5>${tarefa.titulo}</h5>
+        <p>${tarefa.descricao}</p>
+        <div class="actions">
+          <box-icon name='trash' onclick="removerTarefa(${tarefa.id})"></box-icon>
+        </div>
+      
+      </li>
+      `;
+    })
+  }
+}
+      
